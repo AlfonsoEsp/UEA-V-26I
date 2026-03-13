284634e6e65f2131b0642bb8d4490c5c31298c7c
AlfonsoEsp <luislap483>
2026-03-13 12:35:12 -0600

    si

---
+++ JSON/json.js
@@ -0,0 +1,9 @@
+// BUSCAR RESPUESTA EN JSON.JSON
+fetch('json.json')
+    .then(response => response.json())  
+    .then(json => {
+        document.getElementById('nombre').textContent = json.nombre;
+        document.getElementById('edad').textContent = json.edad;
+        document.getElementById('ciudad').textContent = json.ciudad;    
+        console.log(json);
+    })
